7430e821e497d67adaf79d1ce821e239e8df995d
cpscott1 <thacam18gmail.com>
2020-08-04 22:13:48 -0500

    working on getting tests to pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
+        # self.size = 0
+        # self.storage = []
+        self.queue = [None] * capacity
+        self.tail = -1
+        self.head = 0
         self.size = 0
-        self.storage = []
-
     def append(self, item):
-        if self.storage.length < self.capacity:
-            self.storage.append(item)
-        else:
-            self.storage.pop(0)
-    def get(self, node):
-        return self.node
+        if self.size != self.capacity:
+            self.tail = (self.tail + 1) % self.capacity
+            self.queue[self.tail] = item
+            self.size += 1
+    def get(self):
+        if self.size is not None:
+            index = self.head
+            for i in range(self.size):
+                print(self.queue[index])
+                index = (index + 1) % self.capacity
