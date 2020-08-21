fa407076811ce8da190cdf61313483831c8380b3
cpscott1 <thacam18gmail.com>
2020-08-20 23:19:00 -0500

    got two failures still on tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,14 +3,27 @@ class RingBuffer:
         self.capacity = capacity
         self.size = 0
         self.storage = []
-
+    
     def append(self, item):
         if len(self.storage) == self.capacity:
             self.size = 0
             self.storage[self.size] = item
             self.size = (self.size + 1) % self.capacity
+
+
         else:
             self.storage.append(item)
+
+
+    # def append(self, item):
+    #     if len(self.storage) == self.capacity:
+    #         self.size = 0
+    #         self.storage[self.size] = item
+    #         self.size = (self.size + 1) % self.capacity
+    #
+    #
+    #     else:
+    #         self.storage.insert(0, item)
 
     def get(self):
         return self.storage
